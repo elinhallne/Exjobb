954adc6345b32a5fd6f54900fbb0155484650103
pijey97 <36404034+pijey97@users.noreply.github.com>
2021-03-23 16:39:14 +0100

    Fixat options

---
+++ FetaOst/Assets/SettingsMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropDown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+       resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int CurrentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = CurrentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
+    public void SetVolume (float volume)
+    {
+       // Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
