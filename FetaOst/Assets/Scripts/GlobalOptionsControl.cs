4cdd6c5125c21117e98ddebadec3da0020496d50
Elin Hallne <53041041+elinhallne@users.noreply.github.com>
2021-04-30 15:25:45 +0200

    Fixed lolo

--- FetaOst/Assets/Scripts/GlobalOptionsControl.cs
+++ FetaOst/Assets/Scripts/GlobalOptionsControl.cs
@@ -10,11 +10,14 @@ public class GlobalOptionsControl : MonoBehaviour
 
     //Från ChangeText
     public int counter;
-    
+   // public int textColourCount;
+   // public Color currentColour;
+
     //från MenySettings
     public float volume;
-    
 
+
+    
     void Awake()
     {
         if (Instance == null)
