f81b714ee13f25e4d96c581a59e91dcc48337d7d
Elin Hallne <53041041+elinhallne@users.noreply.github.com>
2021-05-04 14:38:00 +0200

    Added last sound

---
+++ FetaOst/Assets/Ljud/AudioControllMeny.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControllMeny : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void CheckIfAudioPlays()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+}
